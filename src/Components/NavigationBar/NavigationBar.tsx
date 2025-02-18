4f78a596b0bacf8e9be5df069a8b6c92a9b07796
Vani85 <vani.natraj@gmail.com>
2025-02-18 16:15:18 +0000

    #6-Added-css

--- src/Components/NavigationBar/NavigationBar.tsx
+++ src/Components/NavigationBar/NavigationBar.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import "./NavigationBar.scss";
 
-function NavigationBar() {
-
-
+function NavigationBar() {    
+    const showMenu = () => {
+        const divContainer = document.getElementById("navigation-links-container");
+        (divContainer as HTMLElement).style.display = 'flex';
+        const elements = document.getElementsByClassName("navigation-link");
+        for (let i = 0; i < elements.length; i++) {
+            (elements[i] as HTMLElement).style.display = 'flex';
+        }
+    }
 return (
     <>
         <div className="mobile-menu">
-            <button className='hamburger-button'>&#9776;</button>
+            <button className='hamburger-button' onClick={showMenu}>&#9776;</button>
         </div>
 
-        <div className="desktop-menu">
-            <Link to="/home"> Home </Link>
-            <Link to="/rover/Perseverance"> Perseverance</Link> 
-            <Link to="/rover/Curiousity"> Curiousity </Link>
-            <Link to="/rover/Spirit"> Spirit </Link> 
-            <Link to="/rover/Opportunity"> Opportunity </Link> 
+        <div className="navigation-links-container" id="navigation-links-container">
+            <Link to="/home" className='navigation-link'> Home </Link>
+            <Link to="/rover/Perseverance" className='navigation-link'> Perseverance</Link> 
+            <Link to="/rover/Curiousity" className='navigation-link'> Curiousity </Link>
+            <Link to="/rover/Spirit" className='navigation-link'> Spirit </Link> 
+            <Link to="/rover/Opportunity" className='navigation-link'> Opportunity </Link> 
         </div>
     </>  
     );
