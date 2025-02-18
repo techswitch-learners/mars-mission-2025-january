44cd44ec0d99696e398e9833c9bb7494f3f64606
Preeti Mishra <preeti.mishra@softwire.com>
2025-02-18 20:49:55 +0000

    #2-fixed prettier rule and fixed eslint error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,14 +4,22 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintPluginPrettierRecommended,
-  
+  {
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "lf",
+        },
+      ],
+    },
+  },
 ];
