c65dbc6ee49d5578ef1b5076170743ec8d1ab650
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-02-18 15:31:59 +0000

    #2 add eslint configuration file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import pluginReact from "eslint-plugin-react";
 
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,jsx,mjs,ts,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
+  pluginReact.configs.flat.recommended,
 ];
