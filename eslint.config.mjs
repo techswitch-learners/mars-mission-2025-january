ef17d37c8c7215ff457b1a1ef9a29a2415cb8897
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-02-18 15:47:38 +0000

    #2 Change eslint to check all files in src

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+  
 ];
