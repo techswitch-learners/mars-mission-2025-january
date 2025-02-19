f988ae7903bef498b4798c889012ead7091cf3b3
Preeti Mishra <preeti.mishra@softwire.com>
2025-02-19 10:30:24 +0000

    #2-Remove commented code from eslint.config.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,6 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
