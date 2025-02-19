737b3238b4186330f83cb186a18e27e2dacec29d
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-02-19 15:39:35 +0000

    Added footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Footer.scss";
+
+function Footer() {
+    return <div>
+    <footer className='footer'>
+        <p>Images provided by NASA Open API</p>
+    </footer>
+    </div>
+};
+
+
+export default Footer;
