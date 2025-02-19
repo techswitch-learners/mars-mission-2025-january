314fd6c23c59c78b866c023f0ca61aa567a818c1
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-02-19 16:17:14 +0000

    #14-footer with added lint

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 import "./Footer.scss";
 
 function Footer() {
-    return <div>
-    <footer className='footer'>
+  return (
+    <div>
+      <footer className="footer">
         <p>Images provided by NASA Open API</p>
-    </footer>
+      </footer>
     </div>
-};
-
+  );
+}
 
 export default Footer;
