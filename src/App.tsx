5b0927906e649ec123a2a0a100289d85bd860d24
Suet Kee <suet.kee@softwire.com>
2025-02-19 14:53:57 +0000

    #4Homepage-component-and-routing

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/rover/:roverName" element={<Rover />} />
       </Routes>
     </Router>
   );
--- src/pages/homepage/Homepage.spec.tsx
+++ src/pages/homepage/Homepage.spec.tsx
@@ -4,8 +4,7 @@ import userEvent from "@testing-library/user-event";
 import { MemoryRouter,Router, Routes, Route } from "react-router";
 import "@testing-library/jest-dom";
 import Home from "./Homepage.tsx";
-import Rover from "../rover/Rover.tsx";
-
+import { createMemoryHistory, MemoryHistory } from "history";
 
 describe("HomePage", () => {
   it("renders correctly", () => {
@@ -18,25 +17,27 @@ describe("HomePage", () => {
     expect(screen.getByText("Mars Rover Explorer")).toBeInTheDocument();
   });
 
+
   it("navigates to the correct rover page when a rover is clicked", async () => {
-      render(
-      <MemoryRouter initialEntries={['/']}>
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/rover/:roverName" element={<Rover />} />
-      </Routes>
-      </MemoryRouter>);
-      
-      const user = userEvent.setup();
+   const history: MemoryHistory = createMemoryHistory({ initialEntries: ['/']});
+    render(
+      <MemoryRouter history = {history}>
+        <Routes> 
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MemoryRouter>
+    );
+  
+  const user = userEvent.setup();
+  const roverlink = screen.getByRole('link', {name: /CURIOSITY/i})
+  user.click(roverlink);
 
-      const roverlink = screen.getByRole('link', {name: /CURIOSITY/i})
-      await user.click(roverlink);
-      
-      expect(screen.getByText(/curiosity/i)).toBeInTheDocument();
-
+  expect(roverlink.getAttribute('href')).toBe("/rover/curiosity");
 
 });
 });
+
+
 
 
 
