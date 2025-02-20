29df20e812cad17c40d7b9819e6740b0b9f298da
Vani85 <vani.natraj@gmail.com>
2025-02-20 15:41:54 +0000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <NavigationBa />
+        <NavigationBar />
         <Routes></Routes>
         <Footer />
       </Router>
