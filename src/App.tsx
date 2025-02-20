667daacfd91b011aaeb21aa0d42b63cfbab6ae28
Jamilya Aitakunova <jamilya.aitakunova@softwire.com>
2025-02-20 12:39:52 +0000

    renamed folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.scss";
-import NavigationBar from "./Components/NavigationBar/NavigationBar.tsx";
+import NavigationBar from "./components/navigationBar/NavigationBar.tsx";
 import { BrowserRouter as Router, Routes } from "react-router";
-import Footer from "./Components/Footer/Footer.tsx";
+import Footer from "./components/footer/Footer.tsx";
 
 function App() {
   return (
