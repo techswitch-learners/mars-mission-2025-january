0cd0dcfe08fb650e21d2c58f43905583d4650ca9
Laila Khan <lailatariqkhan@gmail.com>
2025-02-20 14:49:54 +0000

    #4Homepage-component-and-routing fixing ci fails

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.scss";
 import NavigationBar from "./components/navigationBar/NavigationBar.tsx";
 import { BrowserRouter as Router, Routes } from "react-router";
+import { Route } from "react-router";
 import Footer from "./components/footer/Footer.tsx";
 import Home from "./pages/homepage/Homepage.tsx";
 
