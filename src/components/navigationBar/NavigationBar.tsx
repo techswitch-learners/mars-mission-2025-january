768511a2590e5a95911620fd3e287faf2d8fa3d1
Vani85 <vani.natraj@gmail.com>
2025-02-21 12:14:37 +0000

    mobile fix

--- src/components/navigationBar/NavigationBar.tsx
+++ src/components/navigationBar/NavigationBar.tsx
@@ -11,41 +11,45 @@ function NavigationBar() {
 
   return (
     <>
-      <div className="mobile-menu">
-        <button
-          className="hamburger-open-button"
-          aria-label="Open navigation menu"
-          onClick={toggleMenuDisplayForMobile}
-        >
-          &#9776;
-        </button>
-      </div>
-
-      <div className={`navigation-links-container ${isMenuOpen ? "show" : ""}`}>
-        <div className="mobile-menu-close">
+      <div className="navigation-menu">
+        <div className="mobile-menu">
           <button
-            className="hamburger-close-button"
-            aria-label="Close navigation menu"
+            className="hamburger-open-button"
+            aria-label="Open navigation menu"
             onClick={toggleMenuDisplayForMobile}
           >
-            &times;
+            &#9776;
           </button>
         </div>
-        <Link to="/" className="navigation-link">
-          Home
-        </Link>
-        <Link to="/rover/perseverance" className="navigation-link">
-          Perseverance
-        </Link>
-        <Link to="/rover/curiosity" className="navigation-link">
-          Curiosity
-        </Link>
-        <Link to="/rover/spirit" className="navigation-link">
-          Spirit
-        </Link>
-        <Link to="/rover/opportunity" className="navigation-link">
-          Opportunity
-        </Link>
+
+        <div
+          className={`navigation-links-container ${isMenuOpen ? "show" : ""}`}
+        >
+          <div className="mobile-menu-close">
+            <button
+              className="hamburger-close-button"
+              aria-label="Close navigation menu"
+              onClick={toggleMenuDisplayForMobile}
+            >
+              &times;
+            </button>
+          </div>
+          <Link to="/" className="navigation-link">
+            Home
+          </Link>
+          <Link to="/rover/perseverance" className="navigation-link">
+            Perseverance
+          </Link>
+          <Link to="/rover/curiosity" className="navigation-link">
+            Curiosity
+          </Link>
+          <Link to="/rover/spirit" className="navigation-link">
+            Spirit
+          </Link>
+          <Link to="/rover/opportunity" className="navigation-link">
+            Opportunity
+          </Link>
+        </div>
       </div>
     </>
   );
